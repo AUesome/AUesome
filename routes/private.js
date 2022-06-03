4bd1e7083f58d01755a8a7c795f92953bd3bc264
Anshul Gupta <hi.anshulg@gmail.com>
2022-06-02 18:23:57 -0700

    hi

--- routes/private.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getPrivateRoute } = require("../controllers/private");
-const { protect } = require("../middleware/auth");
-
-router.route("/dashboard").get(protect, getPrivateRoute);
-
-module.exports = router;
---
+++ src/pages/Home.js
@@ -0,0 +1,60 @@
+import "tailwindcss/dist/base.css";
+import "styles/globalStyles.css";
+import React from "react";
+import tw from "twin.macro"; //eslint-disable-line
+import { css } from "styled-components/macro"; //eslint-disable-line
+import {
+  SectionHeading,
+  Subheading as SubheadingBase,
+} from "components/misc/Headings.js";
+
+import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+import MainFeature1 from "components/features/TwoColWithButton.js";
+import Features3 from "components/features/VerticalWithAlternateImageAndText.js";
+import MainFeature3 from "components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
+import Testimonial2 from "components/testimonials/SimplePrimaryBackground.js";
+import Hero from "components/hero/TwoColumnWithVideo.js";
+import MainFeature from "components/features/TwoColSingleFeatureWithStats2.js";
+import Footer from "components/footers/MiniCenteredFooter.js";
+
+const Subheading = tw.span`uppercase tracking-wider text-sm`;
+
+export default () => {
+  return (
+    <AnimationRevealPage disabled>
+      <Hero />
+      <MainFeature />
+      <MainFeature1
+        subheading={<Subheading>About Treact</Subheading>}
+        heading="We are a modern design agency."
+        buttonRounded={false}
+        primaryButtonText="See Portfolio"
+        imageSrc="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80"
+      />
+      <MainFeature1
+        subheading={<Subheading>Our Vision</Subheading>}
+        heading="We aim to disrupt the design space."
+        buttonRounded={false}
+        primaryButtonText="Contact Us"
+        imageSrc="https://images.unsplash.com/3/doctype-hi-res.jpg?ixlib=rb-1.2.1&auto=format&fit=crop&w=768&q=80"
+        textOnLeft={false}
+      />
+      <MainFeature1
+        subheading={<Subheading>About Treact</Subheading>}
+        heading="We are a modern design agency."
+        buttonRounded={false}
+        primaryButtonText="See Portfolio"
+        imageSrc="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80"
+      />
+      <MainFeature1
+        subheading={<Subheading>About Treact</Subheading>}
+        heading="We are a modern design agency."
+        buttonRounded={false}
+        primaryButtonText="See Portfolio"
+        imageSrc="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80"
+      />
+
+      <Footer />
+    </AnimationRevealPage>
+  );
+};
